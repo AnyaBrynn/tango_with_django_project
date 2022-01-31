21ce9ca9eec4ac62e6ca611d8b1ecc0a963e8f58
Anya Stiefbold <2598511s@student.gla.ac.uk>
2022-01-31 10:02:48 +0000

    Chapter 4 and 5 exercises

--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,9 @@ from rango import views
 
 app_name = 'rango'
 urlpatterns = [
-    path(' ', views.index, name = 'index'),
+    path('', views.index, name = 'index'),
+    path('about/', views.about, name = 'about'),
     ]
+
+
+
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
+# Import the Category model
+from rango.models import Category 
 
 #section 3.4 creating a view 
-from django.http import HttpResponse 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    #construct a dictionary to pass to the template engine as its context
+    #the key boldmessage matches to {{ boldmessage }} in the template
+    context_dict = {'boldmessage' : 'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    #return a rendered response to the client
+    #first paramterer is the template we want to use 
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'boldmessage' : 'This tutorial has been put together by Anya'}
+    return render(request, 'rango/about.html', context=context_dict) #HttpResponse('Rango says here is the about page. <a href="/rango/">Index</a>')
